ecb37dc90de2ff3bb19be844de918c47c0016004
Ujjwal Sharma <ujjwal3sharma@gmail.com>
2021-05-30 22:42:52 +0530

    file upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body, Request
+from fastapi import FastAPI, Body, Request, File, UploadFile, Form
 from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
@@ -18,6 +18,14 @@ class NameValue(BaseModel):
 @app.get("/home/{user_name}", response_class=HTMLResponse)
 def write_home(request: Request, user_name: str):
     return templates.TemplateResponse('home.html', {"request": request, "username": user_name})
+
+
+@app.post("/submitform")
+async def handle_form(assigment: str = Form(...), assigment_file: UploadFile = File(...)):
+    print(assigment)
+    print(assigment_file.filename)
+    content_assignment = await assigment_file.read()
+    print(content_assignment)
 
 
 @app.post('/postData')
