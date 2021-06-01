27e68540ee76a3cafccbe66c0756efb0d3a3355b
Ujjwal Sharma <ujjwal3sharma@gmail.com>
2021-06-01 10:45:02 +0530

    Oauth2 token genrated

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI, Body, Request, File, UploadFile, Form
+from fastapi import FastAPI, Body, Request, File, UploadFile, Form, Depends
 from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, oauth2
 app = FastAPI()
 
 list_of_username = []
 templates = Jinja2Templates(directory='templates')
+
+oauth_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 class NameValue(BaseModel):
@@ -13,6 +16,24 @@ class NameValue(BaseModel):
     country: str
     age: int
     base_salary: float
+
+
+@app.post("/token")
+async def token_generate(from_data: OAuth2PasswordRequestForm = Depends()):
+    print(from_data)
+    return {
+        "access token": from_data.username,
+        "token_type": "bearer"
+    }
+
+
+@app.get("/users/profilepic")
+async def profilepic(token: str = Depends(oauth_scheme)):
+    print(token)
+    return {
+        "user": "Ujjwal",
+        "profilepic": "my_face"
+    }
 
 
 @app.get("/home/{user_name}", response_class=HTMLResponse)
